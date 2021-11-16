4d03a1c23cb709532b5cfc1a447db4afb1cde242
Tim Helmke <timhelmke@aol.com>
2021-11-16 14:52:05 +0100

    doc: add LPFormField and LPForm

---
+++ src/components/LPFormField/LPFormFieldWithBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <h2>Hier steht die Frage</h2>
+    <LPSlideBar />
+    <textarea placeholder="optional"></textarea>
+  </section>
+</template>
+
+<script>
+import LPSlideBar from "@/components/LPFormField/LPSlideBar.vue";
+export default {
+  name: "LPFormFieldWithBar",
+  components: {
+    LPSlideBar,
+  },
+};
+</script>
+
+<style></style>
