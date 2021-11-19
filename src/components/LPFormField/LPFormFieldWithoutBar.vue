d80b1c2b137b73a93885fac0eaf4825395d15a26
Tim Helmke <timhelmke@aol.com>
2021-11-19 08:52:00 +0100

    feature: add v-model logic from child to parent

--- src/components/LPFormField/LPFormFieldWithoutBar.vue
+++ src/components/LPFormField/LPFormFieldWithoutBar.vue
@@ -1,7 +1,12 @@
 <template>
   <section>
     <legend>{{ lpLegend }}</legend>
-    <textarea :placeholder="lp__placeholder"></textarea>
+    <textarea
+      :placeholder="lp__placeholder"
+      :v-model="modelValue"
+      @keydown="updateValue"
+      @keyup="updateValue"
+    ></textarea>
   </section>
 </template>
 
@@ -14,6 +19,14 @@ export default {
     },
     lp__placeholder: {
       type: String,
+    },
+    modelValue: {
+      type: [String, Number],
+    },
+    methods: {
+      updateValue() {
+        this.$emit("lp-value", event.target.value);
+      },
     },
   },
 };
