1da67f0c671602ac2c6bc408ad2a01d7de113dd7
Dmitry Kolesnikov <dim.kolesnikov@gmail.com>
2022-02-08 13:24:46 +0100

    create basic slides and footer

--- src/views/Slides.vue
+++ src/views/Slides.vue
@@ -1,8 +1,87 @@
 <template>
-  <div>
-    <h2>Slides</h2>
-  </div>
+  <header>
+    <h1>Slides</h1>
+  </header>
+  <main>
+    <h2>Web Dev Foundation</h2>
+    <li>
+      <a
+        href="https://docs.google.com/presentation/d/1MgywvbSmj7TGK4zSdQDSAg1reqlp9K__tgJEkXlTs-8"
+        rel="nofollow"
+        >Developer Setup</a
+      >
+    </li>
+    <li>
+      <a
+        href="https://docs.google.com/presentation/d/1-PaZptKth8AEABs4wyTCjcLZ8FcV14o4leRMkS_9mCI"
+        rel="nofollow"
+        >First Steps with Git</a
+      >
+    </li>
+    <li>
+      <a
+        href="https://docs.google.com/presentation/d/1wG3SoVjLjX-EFf0Nho_d1pP786muJ4cDXt0DJt3QCSk"
+        rel="nofollow"
+        >HTML Foundation</a
+      >
+    </li>
+    <li>
+      <a
+        href="https://docs.google.com/presentation/d/1zFr2nDVndzVRG-alTnwAieifHYblnIkuLKDYdghvRoc"
+        rel="nofollow"
+        >CSS Foundation</a
+      >
+    </li>
+    <li>
+      <a
+        href="https://docs.google.com/presentation/d/1nD1b1FSYWWng31gTIOpCN9T79orE6lSTbzcRobbmkaE"
+        rel="nofollow"
+        >Next Steps with Git</a
+      >
+    </li>
+    <h2>Coding Foundation</h2>
+    <li>
+      <a
+        href="https://docs.google.com/presentation/d/1nD1b1FSYWWng31gTIOpCN9T79orE6lSTbzcRobbmkaE/"
+        rel="nofollow"
+        >Developer Setup</a
+      >
+    </li>
+    <li>
+      <a
+        href="https://docs.google.com/presentation/d/1BoHnEwILVQOSAFEi_y6ZDI-oCEozLfCdlHnrVmySYx0/"
+        rel="nofollow"
+        >Solving Problems with JavaScript</a
+      >
+    </li>
+    <h2>Web Apps Foundation</h2>
+    <li>
+      <a
+        href="https://docs.google.com/presentation/d/1uD4eFnXEOkJOTPgzSQNdMepNH7N8swBJH5JvLNsnpgs/"
+        rel="nofollow"
+        >Web App Basics</a
+      >
+    </li>
+    <h2>Restful Backends</h2>
+    <li>
+      <a
+        href="https://docs.google.com/presentation/d/1TrjUk7nu9qUTmvqlRl-KoqM5dtf9dc8H5cfQ3SncesE/"
+        rel="nofollow"
+        >Fetch API and HTTP</a
+      >
+    </li>
+    <h2>Web Apps with Vue.js</h2>
+    <li>
+      <a
+        href="https://docs.google.com/presentation/d/16fXnSNeVMWUvHmv24SwoO4rKDG_1dWUem4UJ7UKVduw/edit#slide=id.gd1c16f7fa1_1_28"
+        rel="nofollow"
+        >Vue Basics</a
+      >
+    </li>
+  </main>
 </template>
+
+<style scoped></style>
 
 <script>
 export default {
