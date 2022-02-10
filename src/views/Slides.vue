8f1a741fc98a96b1b26eec5a1a57a89db7913036
Dmitry Kolesnikov <dim.kolesnikov@gmail.com>
2022-02-10 10:53:34 +0100

    finishes style

--- src/views/Slides.vue
+++ src/views/Slides.vue
@@ -1,261 +1,302 @@
 <template>
-  <h1>Slides</h1>
-  <h2>Web Dev Foundation</h2>
+  <h2>Slides</h2>
+  <h3>Web Dev Foundation</h3>
   <p>4 WEEKS</p>
   <li>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
-      fill="currentColor"
-      class="bi bi-card-text"
-      viewBox="0 0 16 14"
-    >
-      <path
-        d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
-      />
-      <path
-        d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-      />
-    </svg>
-    <a
-      href="https://docs.google.com/presentation/d/1MgywvbSmj7TGK4zSdQDSAg1reqlp9K__tgJEkXlTs-8"
-      target="_blank"
-      rel="nofollow"
-      >Developer Setup</a
-    >
+    <div class="icon-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50"
+        fill="currentColor"
+        class="bi bi-card-text"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
+        />
+        <path
+          d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    </div>
+    <div class="link-block">
+      <a
+        href="https://docs.google.com/presentation/d/1MgywvbSmj7TGK4zSdQDSAg1reqlp9K__tgJEkXlTs-8"
+        target="_blank"
+        rel="nofollow"
+        >Developer Setup</a
+      >
+    </div>
   </li>
   <li>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
-      fill="currentColor"
-      class="bi bi-card-text"
-      viewBox="0 0 16 14"
-    >
-      <path
-        d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
-      />
-      <path
-        d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-      />
-    </svg>
-    <a
-      href="https://docs.google.com/presentation/d/1-PaZptKth8AEABs4wyTCjcLZ8FcV14o4leRMkS_9mCI"
-      target="_blank"
-      rel="nofollow"
-    >
-      First Steps with Git</a
-    >
+    <div class="icon-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50"
+        fill="currentColor"
+        class="bi bi-card-text"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
+        />
+        <path
+          d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    </div>
+    <div class="link-block">
+      <a
+        href="https://docs.google.com/presentation/d/1-PaZptKth8AEABs4wyTCjcLZ8FcV14o4leRMkS_9mCI"
+        target="_blank"
+        rel="nofollow"
+      >
+        First Steps with Git</a
+      >
+    </div>
   </li>
   <li>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
-      fill="currentColor"
-      class="bi bi-card-text"
-      viewBox="0 0 16 14"
-    >
-      <path
-        d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
-      />
-      <path
-        d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-      />
-    </svg>
-    <a
-      href="https://docs.google.com/presentation/d/1wG3SoVjLjX-EFf0Nho_d1pP786muJ4cDXt0DJt3QCSk"
-      target="_blank"
-      rel="nofollow"
-      >HTML Foundation</a
-    >
+    <div class="icon-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50"
+        fill="currentColor"
+        class="bi bi-card-text"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
+        />
+        <path
+          d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    </div>
+    <div class="link-block">
+      <a
+        href="https://docs.google.com/presentation/d/1wG3SoVjLjX-EFf0Nho_d1pP786muJ4cDXt0DJt3QCSk"
+        target="_blank"
+        rel="nofollow"
+        >HTML Foundation</a
+      >
+    </div>
   </li>
   <li>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
-      fill="currentColor"
-      class="bi bi-card-text"
-      viewBox="0 0 16 14"
-    >
-      <path
-        d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
-      />
-      <path
-        d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-      />
-    </svg>
-    <a
-      href="https://docs.google.com/presentation/d/1zFr2nDVndzVRG-alTnwAieifHYblnIkuLKDYdghvRoc"
-      target="_blank"
-      rel="nofollow"
-      >CSS Foundation</a
-    >
+    <div class="icon-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50"
+        fill="currentColor"
+        class="bi bi-card-text"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
+        />
+        <path
+          d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    </div>
+    <div class="link-block">
+      <a
+        href="https://docs.google.com/presentation/d/1zFr2nDVndzVRG-alTnwAieifHYblnIkuLKDYdghvRoc"
+        target="_blank"
+        rel="nofollow"
+        >CSS Foundation</a
+      >
+    </div>
   </li>
   <li>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
-      fill="currentColor"
-      class="bi bi-card-text"
-      viewBox="0 0 16 14"
-    >
-      <path
-        d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
-      />
-      <path
-        d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-      />
-    </svg>
-    <a
-      href="https://docs.google.com/presentation/d/1nD1b1FSYWWng31gTIOpCN9T79orE6lSTbzcRobbmkaE"
-      target="_blank"
-      rel="nofollow"
-      >Next Steps with Git</a
-    >
+    <div class="icon-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50"
+        fill="currentColor"
+        class="bi bi-card-text"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
+        />
+        <path
+          d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    </div>
+    <div class="link-block">
+      <a
+        href="https://docs.google.com/presentation/d/1nD1b1FSYWWng31gTIOpCN9T79orE6lSTbzcRobbmkaE"
+        target="_blank"
+        rel="nofollow"
+        >Next Steps with Git</a
+      >
+    </div>
   </li>
-  <h2>Coding Foundation</h2>
+  <h3>Coding Foundation</h3>
   <p>13 DAYS</p>
   <li>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
-      fill="currentColor"
-      class="bi bi-card-text"
-      viewBox="0 0 16 14"
-    >
-      <path
-        d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
-      />
-      <path
-        d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-      />
-    </svg>
-    <a
-      href="https://docs.google.com/presentation/d/1nD1b1FSYWWng31gTIOpCN9T79orE6lSTbzcRobbmkaE/"
-      target="_blank"
-      rel="nofollow"
-      >Developer Setup</a
-    >
+    <div class="icon-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50"
+        fill="currentColor"
+        class="bi bi-card-text"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
+        />
+        <path
+          d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    </div>
+    <div class="link-block">
+      <a
+        href="https://docs.google.com/presentation/d/1nD1b1FSYWWng31gTIOpCN9T79orE6lSTbzcRobbmkaE/"
+        target="_blank"
+        rel="nofollow"
+        >Developer Setup</a
+      >
+    </div>
   </li>
   <li>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
-      fill="currentColor"
-      class="bi bi-card-text"
-      viewBox="0 0 16 14"
-    >
-      <path
-        d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
-      />
-      <path
-        d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-      />
-    </svg>
-    <a
-      href="https://docs.google.com/presentation/d/1BoHnEwILVQOSAFEi_y6ZDI-oCEozLfCdlHnrVmySYx0/"
-      target="_blank"
-      rel="nofollow"
-      >Solving Problems with JavaScript</a
-    >
+    <div class="icon-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50"
+        fill="currentColor"
+        class="bi bi-card-text"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
+        />
+        <path
+          d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    </div>
+    <div class="link-block">
+      <a
+        href="https://docs.google.com/presentation/d/1BoHnEwILVQOSAFEi_y6ZDI-oCEozLfCdlHnrVmySYx0/"
+        target="_blank"
+        rel="nofollow"
+        >Solving Problems with JavaScript</a
+      >
+    </div>
   </li>
-  <h2>Web Apps Foundation</h2>
+  <h3>Web Apps Foundation</h3>
   <p>9 DAYS</p>
   <li>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
-      fill="currentColor"
-      class="bi bi-card-text"
-      viewBox="0 0 16 14"
-    >
-      <path
-        d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
-      />
-      <path
-        d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-      />
-    </svg>
-    <a
-      href="https://docs.google.com/presentation/d/1uD4eFnXEOkJOTPgzSQNdMepNH7N8swBJH5JvLNsnpgs/"
-      target="_blank"
-      rel="nofollow"
-      >Web App Basics</a
-    >
+    <div class="icon-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50"
+        fill="currentColor"
+        class="bi bi-card-text"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
+        />
+        <path
+          d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    </div>
+    <div class="link-block">
+      <a
+        href="https://docs.google.com/presentation/d/1uD4eFnXEOkJOTPgzSQNdMepNH7N8swBJH5JvLNsnpgs/"
+        target="_blank"
+        rel="nofollow"
+        >Web App Basics</a
+      >
+    </div>
   </li>
-  <h2>Restful Backends</h2>
+  <h3>Restful Backends</h3>
   <p>9 DAYS</p>
   <li>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
-      fill="currentColor"
-      class="bi bi-card-text"
-      viewBox="0 0 16 14"
-    >
-      <path
-        d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
-      />
-      <path
-        d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-      />
-    </svg>
-    <a
-      href="https://docs.google.com/presentation/d/1TrjUk7nu9qUTmvqlRl-KoqM5dtf9dc8H5cfQ3SncesE/"
-      target="_blank"
-      rel="nofollow"
-      >Fetch API and HTTP</a
-    >
+    <div class="icon-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50"
+        fill="currentColor"
+        class="bi bi-card-text"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
+        />
+        <path
+          d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    </div>
+    <div class="link-block">
+      <a
+        href="https://docs.google.com/presentation/d/1TrjUk7nu9qUTmvqlRl-KoqM5dtf9dc8H5cfQ3SncesE/"
+        target="_blank"
+        rel="nofollow"
+        >Fetch API and HTTP</a
+      >
+    </div>
   </li>
-  <h2>Web Apps with Vue.js</h2>
+  <h3>Web Apps with Vue.js</h3>
   <p>11 DAYS</p>
   <li>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="50"
-      height="50"
-      fill="currentColor"
-      class="bi bi-card-text"
-      viewBox="0 0 16 14"
-    >
-      <path
-        d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
-      />
-      <path
-        d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-      />
-    </svg>
-    <a
-      href="https://docs.google.com/presentation/d/16fXnSNeVMWUvHmv24SwoO4rKDG_1dWUem4UJ7UKVduw/edit#slide=id.gd1c16f7fa1_1_28"
-      target="_blank"
-      rel="nofollow"
-      >Vue Basics</a
-    >
+    <div class="icon-block">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50"
+        fill="currentColor"
+        class="bi bi-card-text"
+        viewBox="0 0 16 16"
+      >
+        <path
+          d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
+        />
+        <path
+          d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
+        />
+      </svg>
+    </div>
+    <div class="link-block">
+      <a
+        href="https://docs.google.com/presentation/d/16fXnSNeVMWUvHmv24SwoO4rKDG_1dWUem4UJ7UKVduw/edit#slide=id.gd1c16f7fa1_1_28"
+        target="_blank"
+        rel="nofollow"
+        >Vue Basics</a
+      >
+    </div>
   </li>
 </template>
 
 <style scoped>
-h1 {
+h2 {
   margin-left: 2rem;
   color: grey;
 }
-h2 {
+h3 {
   display: inline-block;
   margin-left: 2rem;
+  text-transform: uppercase;
 }
 p {
   float: right;
   margin-right: 2rem;
-  margin-top: 26px;
+  margin-top: 21px;
 }
 li {
   list-style-type: none;
@@ -264,17 +305,29 @@ li {
   border-radius: 5px;
   background-color: rgb(219, 219, 219);
   margin: 10px 2rem;
-  padding: 0.5rem;
+  display: flex;
+  _justify-content: space-between;
+  _padding: 0.5rem;
 }
 a {
   text-decoration: none;
-  padding: 40px;
+  _padding-top: 11px;
+  _padding-right: 150px;
   color: black;
   font-size: 20px;
 }
 svg {
+  _padding-left: 30px;
+}
+
+.link-block {
+  padding-top: 17px;
+  margin-bottom: 15px;
   padding-left: 30px;
-  _padding-top: 5px;
+}
+.icon-block {
+  padding-top: 5px;
+  padding-left: 30px;
 }
 </style>
 
