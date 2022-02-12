8568385e27547c306bdb814f325db6102c4ee560
Nico Koenig <nico.koenig@coding-bootcamps.eu>
2022-02-12 10:06:10 +0100

    feat: adds basic route permissions

---
+++ src/views/Guest.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <header class="page-heading">
+      <h2 class="page-heading__title">GUEST</h2>
+      <p class="page-heading__subtitle">You only have guest permissions.</p>
+    </header>
+    <div class="guest">
+      <p>
+        You currently have a guest status which means you have no access to the
+        app.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Guest",
+};
+</script>
+
+<style lang="css">
+.Guest {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+}
+</style>
