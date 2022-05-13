9fd25aac8bf44cc37fbdfda80e1538c0b137180d
Nico <nico.koenig@coding-bootcamps.eu>
2022-05-13 11:49:36 +0200

    refactor: refactors login with fallback for new all-users collection

--- src/firebase.js
+++ src/firebase.js
@@ -6,6 +6,9 @@ import {
   collection,
   where,
   getDocs,
+  doc,
+  getDoc,
+  setDoc,
 } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import store from "./store";
@@ -35,7 +38,11 @@ if (mode === "development") {
 const onAuthStateChangedPromise = new Promise((resolve, reject) => {
   auth.onAuthStateChanged(
     async (user) => {
-      await store.dispatch(user !== null ? "login" : "logout", user);
+      if (user !== null) {
+        await store.dispatch("login", user);
+      } else {
+        await store.dispatch("logout");
+      }
       resolve();
     },
     (err) => {
@@ -45,20 +52,69 @@ const onAuthStateChangedPromise = new Promise((resolve, reject) => {
 });
 export const onAuthStateInit = () => onAuthStateChangedPromise;
 
-export const loadUserDetails = (user) => {
-  return new Promise((resolve) => {
-    getDocs(
-      query(collection(db, "all-users"), where("uid", "==", user.uid))
-    ).then((docs) => {
-      if (docs.size === 0) {
-        // user record not found
-      } else if (docs.size === 1) {
-        // User record found
-        resolve(docs.docs[0].data());
-      } else {
-        console.error("Multiple entries found for uid ", user.uid);
-      }
-      resolve(undefined);
-    });
-  });
+export const loadUserDetails = async (user) => {
+  // Default: find user document with document.id === user.uid
+  const docRef = doc(db, "all-users", user.uid);
+  const userDoc = await getDoc(docRef);
+  if (userDoc.exists()) {
+    const userData = userDoc.data();
+    return userData;
+  }
+
+  // Fallback: find user document with random documentId and uid === uid
+  const docs = await getDocs(
+    query(collection(db, "all-users"), where("uid", "==", user.uid))
+  );
+
+  if (docs.size === 0) {
+    throw new Error("No user document found for uid ", user.uid);
+  } else if (docs.size === 1) {
+    const userDoc = docs.docs[0];
+    const userData = userDoc.data();
+
+    if (userDoc.id !== user.uid) {
+      createUserDocument(user.uid, userData);
+
+      // const oldDocumentRef = userDoc.ref;
+      // await deleteDoc(oldDocumentRef);
+      // console.info("Deleted old user doc with incorrect id");
+    }
+
+    return userData;
+  } else {
+    throw new Error("Multiple entries found for uid ", user.uid);
+  }
 };
+
+/**
+ * Check if a document is in the all-users collection for a specific uid
+ * @param {*} uid (string)
+ * @returns boolean
+ */
+export async function doesUserRecordExist(uid) {
+  // Check if document exists with documentId === uid
+  const docRef = doc(db, "all-users", uid);
+  const userDoc = await getDoc(docRef);
+  if (userDoc.exists()) {
+    return true;
+  }
+
+  // Fallback Behavior: find documents where uid === uid
+  const docs = await getDocs(
+    query(collection(db, "all-users"), where("uid", "==", uid))
+  );
+  if (docs.size === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+/**
+ * Creates new user document for userCrendentials passed by GitHub authentication provider
+ * @param {*} uid
+ * @param {*} userDetails
+ */
+export async function createUserDocument(uid, userDetails) {
+  await setDoc(doc(db, "all-users", uid), userDetails);
+}
