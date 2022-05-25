9538f7c9f2e183fc92972b91d7867757ac4c8526
Nico <nico.koenig@coding-bootcamps.eu>
2022-05-25 12:21:37 +0200

    refactor: refactors schedule feature
    - schedule of all classes visible for students

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -4,22 +4,18 @@
     <p class="page-heading__subtitle">{{ scheduleHeading }}</p>
   </header>
   <div class="schedule">
-    <div
-      v-if="
-        this.$store.getters.canShowSchedule || this.$store.getters.isTeacher
-      "
-    >
-      <template v-if="this.$store.getters.isTeacher">
-        <select name="" id="selected-class" v-model="selectedClass">
-          <option
-            v-for="className in classes"
-            :key="className"
-            :value="className"
-          >
-            {{ className }}
-          </option>
-        </select>
-      </template>
+    <div>
+      <label for="selected-class">Selected Class</label>&nbsp;
+      <select name="" id="selected-class" v-model="selectedClass">
+        <option
+          v-for="className in classes"
+          :key="className"
+          :value="className"
+        >
+          {{ className.substring(0, 7) }}
+        </option>
+      </select>
+
       <ul>
         <li
           v-for="day in schedule"
@@ -31,15 +27,6 @@
             day.totalTopicDays
           }})
         </li>
-      </ul>
-    </div>
-    <div v-else>
-      No schedule available - reasons can maybe
-      <ul>
-        <li>Your are a part-time student</li>
-        <li>Your are not a student</li>
-        <li>There is no start date assigned in the database</li>
-        <li>This app has a bug 🤷‍♂️</li>
       </ul>
     </div>
   </div>
@@ -63,7 +50,13 @@ export default {
   },
   created() {
     this.classes = classNames;
-    this.selectedClass = this.classes[0];
+    if (this.$store.state.className) {
+      this.selectedClass = classNames.filter((c) =>
+        c.startsWith(this.$store.state.className)
+      )[0];
+    } else {
+      this.selectedClass = this.classes[0];
+    }
   },
   methods: {
     formatDate(d) {
@@ -80,13 +73,7 @@ export default {
   },
   computed: {
     scheduleHeading() {
-      if (this.$store.getters.isStudent) {
-        return "Your personal bootcamp schedule";
-      } else if (this.$store.getters.isTeacher && this.selectedClass) {
-        return "Schedule of Class " + this.selectedClass;
-      } else {
-        return "";
-      }
+      return "Schedule of Class " + this.selectedClass.substring(0, 7);
     },
     daysInBootcamp() {
       let studentStartDate = new Date(this.$store.state.startDate);
@@ -94,16 +81,8 @@ export default {
       return calculateWorkingDaysSinceCampStart(studentStartDate, today);
     },
     schedule() {
-      if (this.$store.getters.isStudent) {
-        let studentStartDate = new Date(this.$store.state.startDate);
-        let className = this.$store.state.className;
-        return calculateSchedule(studentStartDate, className);
-      } else if (this.$store.getters.isTeacher && this.selectedClass) {
-        const className = this.selectedClass.substring(0, 7);
-        return calculateSchedule(new Date(this.selectedClass), className);
-      } else {
-        return [];
-      }
+      const className = this.selectedClass.substring(0, 7);
+      return calculateSchedule(new Date(this.selectedClass), className);
     },
   },
 };
