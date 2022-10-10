c7ac20c31f38be3ab645fd94e4f028073291b3b2
ferniemann <hello@ferdinand-niemann.eu>
2022-10-10 15:52:25 +0200

    add: adds data to detailed schedule

--- src/schedule/schedule-details.js
+++ src/schedule/schedule-details.js
@@ -49,6 +49,158 @@ export const scheduleDetails = {
       "https://docs.google.com/presentation/d/1K0CbUdITLRgG7NlqqC1yXKdNfRjA3gQAdEI8VRBEVKQ",
     days: 4,
   },
+  "Next Steps with Git": {
+    title: "Next Steps with Git",
+    classRoomLink:
+      "https://classroom.coding-bootcamps.eu/products/web-dev-foundation/categories/2149114839",
+    gitHubLink: "",
+    slidesLink:
+      "https://docs.google.com/presentation/d/1iTOCNw9hHCytLA6BE_BidURMVbkG7suCdrkp-Gtu5Eg",
+    days: 1,
+  },
+  "Recap Day Web Dev Foundation": {
+    title: "Recap Day",
+    classRoomLink: "",
+    gitHubLink:
+      "https://github.com/coding-bootcamps-eu/web-dev-foundation/blob/main/katas/kata-student-hub-design.md",
+    slidesLink: "",
+    days: 1,
+  },
+  "Introduction to Programming with p5": {
+    title: "Introduction to Programming with p5",
+    classRoomLink:
+      "https://classroom.coding-bootcamps.eu/products/coding-foundation/categories/2149242927",
+    gitHubLink:
+      "https://github.com/coding-bootcamps-eu/coding-foundation/tree/main/introduction-to-programming-with-p5",
+    slidesLink:
+      "https://docs.google.com/presentation/d/1nD1b1FSYWWng31gTIOpCN9T79orE6lSTbzcRobbmkaE",
+    days: 5,
+  },
+  "Solving Problems with JavaScript": {
+    title: "Solving Problems with JavaScript",
+    classRoomLink:
+      "https://classroom.coding-bootcamps.eu/products/coding-foundation/categories/2149242930",
+    gitHubLink:
+      "https://github.com/coding-bootcamps-eu/coding-foundation/blob/main/solving-problems-with-javascript/01-solve-katas-on-codewars.md",
+    slidesLink:
+      "https://docs.google.com/presentation/d/1BoHnEwILVQOSAFEi_y6ZDI-oCEozLfCdlHnrVmySYx0",
+    days: 4,
+  },
+  "From Objects to Classes": {
+    title: "From Objects to Classes",
+    classRoomLink:
+      "https://classroom.coding-bootcamps.eu/products/coding-foundation/categories/2149346393",
+    gitHubLink: "",
+    slidesLink:
+      "https://docs.google.com/presentation/d/1bzsTYJZsV4PpE6S0qriK1BEV2oTV6oXc105rXFLoPDQ",
+    days: 1,
+  },
+  "Recap Day Coding Foundation": {
+    title: "Recap Day",
+    classRoomLink: "",
+    gitHubLink: "",
+    slidesLink: "",
+    days: 1,
+  },
+  "Web App Basics": {
+    title: "Web App Basics",
+    classRoomLink:
+      "https://classroom.coding-bootcamps.eu/products/web-apps-foundation/categories/2149381295",
+    gitHubLink:
+      "https://github.com/coding-bootcamps-eu/web-apps-foundation/tree/main/web-app-basics",
+    slidesLink:
+      "https://docs.google.com/presentation/d/1uD4eFnXEOkJOTPgzSQNdMepNH7N8swBJH5JvLNsnpgs",
+    days: 5,
+  },
+  "Basic State Management": {
+    title: "Basic State Management",
+    classRoomLink:
+      "https://classroom.coding-bootcamps.eu/products/web-apps-foundation/categories/2149502364",
+    gitHubLink:
+      "https://github.com/coding-bootcamps-eu/web-apps-foundation/tree/main/state-management",
+    slidesLink:
+      "https://docs.google.com/presentation/d/1gx71PtMmQ_7qGm9JQE5YNquAmdewyFriNiOyJSS1ZcY",
+    days: 2,
+  },
+  "Recap Day Web Apps Foundation": {
+    title: "Recap Day",
+    classRoomLink: "",
+    gitHubLink:
+      "https://github.com/coding-bootcamps-eu/web-apps-foundation/blob/main/katas/password-check.md",
+    slidesLink: "",
+    days: 1,
+  },
+  "Fetch and Http Requests": {
+    title: "Fetch and Http Requests",
+    classRoomLink:
+      "https://classroom.coding-bootcamps.eu/products/restful-backends/categories/2149521248",
+    gitHubLink:
+      "https://github.com/coding-bootcamps-eu/restful-backends/tree/main/fetch-api-and-http",
+    slidesLink:
+      "https://docs.google.com/presentation/d/1TrjUk7nu9qUTmvqlRl-KoqM5dtf9dc8H5cfQ3SncesE",
+    days: 2,
+  },
+  "Intro to Node.js and NPM": {
+    title: "Intro to Noe.js and NPM",
+    classRoomLink:
+      "https://classroom.coding-bootcamps.eu/products/restful-backends/categories/2149867723",
+    gitHubLink:
+      "https://github.com/coding-bootcamps-eu/restful-backends/tree/main/introduction-to-node-js",
+    slidesLink:
+      "https://docs.google.com/presentation/d/1OSK3dDMifzDmis497pd0asm52wcfcQ1ipUeFDHrz2hY",
+    days: 1,
+  },
+  "Introduction to Restful APIs": {
+    title: "Introduction to Restful APIs",
+    classRoomLink:
+      "https://classroom.coding-bootcamps.eu/products/restful-backends/categories/2149867725",
+    gitHubLink: "https://github.com/coding-bootcamps-eu/restful-backends",
+    slidesLink:
+      "https://docs.google.com/presentation/d/1FFpnTozAK3zMm7_5T1Qp45YYRNTzPo2OsS72KsF2x4A",
+    days: 3,
+  },
+  "Recap Day Restful Backends": {
+    title: "Recap Day",
+    classRoomLink: "",
+    gitHubLink:
+      "https://github.com/coding-bootcamps-eu/restful-backends/blob/main/katas/linked-in-contacts.md",
+    slidesLink: "",
+    days: 1,
+  },
+  Testing: {
+    title: "Testing",
+    classRoomLink:
+      "https://classroom.coding-bootcamps.eu/products/refactoring-and-testing",
+    gitHubLink:
+      "https://github.com/coding-bootcamps-eu/testing-and-refactoring",
+    slidesLink:
+      "https://docs.google.com/presentation/d/12nai3Z0juEk6m3SSExq9umoVn-Ub8wYpbEh6Sf_iQfo",
+    days: 4,
+  },
+  "Vue.js": {
+    title: "Vue.js",
+    classRoomLink:
+      "https://classroom.coding-bootcamps.eu/products/webapps-with-vuejs",
+    gitHubLink:
+      "https://github.com/coding-bootcamps-eu/web-apps-with-vuejs/tree/main/katas",
+    slidesLink:
+      "https://docs.google.com/presentation/d/16fXnSNeVMWUvHmv24SwoO4rKDG_1dWUem4UJ7UKVduw",
+    days: 10,
+  },
+  "Recap Day Vue.js": {
+    title: "Recap Day",
+    classRoomLink: "",
+    gitHubLink: "",
+    slidesLink: "",
+    days: 1,
+  },
+  Abschlussprojekt: {
+    title: "Abschlussprojekt",
+    classRoomLink: "",
+    gitHubLink: "",
+    slidesLink: "",
+    days: 7,
+  },
   //   "": {
   //     title: "",
   //     classRoomLink:
