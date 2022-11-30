4e30845496e4bb6889306982eab68581fe1a09a4
Nico König <nico.koenig@coding-bootcamps.eu>
2022-11-30 11:20:57 +0100

    feat: adds zoom cloud function

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,7 @@ const {
   setUserClaimsOnUserCreate,
   setUserClaimsOnUserUpdate,
 } = require("./user-claims");
+const { zoomApi } = require("./zoom/zoom");
 
 admin.initializeApp();
 
@@ -13,3 +14,6 @@ exports.studenthub = studenthubApi;
 // User Claims
 exports.setUserClaimsOnUserCreate = setUserClaimsOnUserCreate;
 exports.setUserClaimsOnUserUpdate = setUserClaimsOnUserUpdate;
+
+// Zoom Api
+exports.zoomMeetings = zoomApi;
