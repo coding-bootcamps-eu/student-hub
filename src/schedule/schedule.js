6f0a0a009e7af88385524918fa71d251ea9b5259
ferniemann <hello@ferdinand-niemann.eu>
2023-05-23 08:32:11 +0200

    added new class to schedule

--- src/schedule/schedule.js
+++ src/schedule/schedule.js
@@ -4,7 +4,7 @@ import { scheduleDetails } from "./schedule-details";
 export const bootcampDays = 65;
 
 // Current classes
-export const classNames = ["2023-02-14", "2023-03-14", "2023-04-18"];
+export const classNames = ["2023-03-14", "2023-04-18", "2023-05-23"];
 
 export const defaultSchedule = [
   { topic: "Onboarding", days: 1 },
