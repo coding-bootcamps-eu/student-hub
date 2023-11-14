d1526033bc94345caa3d5c9b80faa13fc3cd725f
Joe Ray Gregory <joe.gregory@coding-bootcamps.eu>
2023-11-14 11:52:49 +0100

    feat: Add report absence links

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -31,6 +31,28 @@
         </ul>
       </section>
       <nav class="home__nav">
+        <a
+          class="home__link"
+          href="https://form.asana.com/?k=msqPDRYEAxA8uspT_xcYbw&d=1181024715195521"
+          target="_blank"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="bi bi-calendar-check"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+            />
+          </svg>
+
+          <span class="home__link-text">Report absence</span>
+        </a>
         <router-link
           class="home__link"
           to="/schedule"
