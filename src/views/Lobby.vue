86de8eb91e73e84b24d5954274ae3a7ee624c31f
Ferdinand Niemann <hello@ferdi.digital>
2024-03-02 21:41:45 +0100

    feat: seperate views for teacher in lobby

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -1,146 +1,71 @@
 <template>
   <section>
     <header class="page-heading">
-      <h2 class="page-heading__title">Lobby</h2>
       <p class="page-heading__subtitle">
         Hello, {{ this.$store.getters.userName }} - Let's go!
       </p>
     </header>
-    <div class="home">
-      <nav class="home__nav">
-        <a
-          class="home__link"
-          href="https://form.asana.com/?k=msqPDRYEAxA8uspT_xcYbw&d=1181024715195521"
-          target="_blank"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="currentColor"
-            class="bi bi-calendar-check"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
-            />
-          </svg>
-
-          <span class="home__link-text">Report absence</span>
-        </a>
-        <router-link class="home__link" to="/schedule">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            class="bi bi-calendar-check"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"
-            />
-            <path
-              d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"
-            />
-          </svg>
-          <span class="home__link-text">Schedule</span>
-        </router-link>
-        <router-link class="home__link" to="/meetings">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            class="bi bi-calendar-week"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z"
-            />
-            <path
-              d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"
-            />
-          </svg>
-          <span class="home__link-text">Meetings</span>
-        </router-link>
-        <router-link class="home__link" to="/recordings">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            class="bi bi-camera-video"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5zm11.5 5.175 3.5 1.556V4.269l-3.5 1.556v4.35zM2 4a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h7.5a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H2z"
-            />
-          </svg>
-          <span class="home__link-text">Recordings</span>
-        </router-link>
-        <router-link class="home__link" to="/slides">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            class="bi bi-card-text"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"
-            />
-            <path
-              d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"
-            />
-          </svg>
-          <span class="home__link-text">Slides</span>
-        </router-link>
-        <router-link class="home__link" to="/students">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            class="bi bi-people"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1h8zm-7.978-1A.261.261 0 0 1 7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002a.274.274 0 0 1-.014.002H7.022zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4zm3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0zM6.936 9.28a5.88 5.88 0 0 0-1.23-.247A7.35 7.35 0 0 0 5 9c-4 0-5 3-5 4 0 .667.333 1 1 1h4.216A2.238 2.238 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816zM4.92 10A5.493 5.493 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275zM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0zm3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4z"
-            />
-          </svg>
-          <span class="home__link-text">Students</span>
-        </router-link>
-        <router-link
-          class="home__link"
-          to="/zoom"
-          v-if="this.$store.getters.hasPermissions"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            class="bi bi-person-workspace"
-            viewBox="0 0 16 16"
-          >
-            <path
-              d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"
-            />
-            <path
-              d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z"
-            />
-          </svg>
-          <span class="home__link-text">Zoom Rooms</span>
-        </router-link>
-      </nav>
+    <div class="student-wrapper" v-if="isStudent">
+      <StudentItems />
+    </div>
+    <div class="teacher-wrapper" v-if="isTeacher">
+      <form class="view-wrapper" @change="selectedView = $event.target.value">
+        <span>Choose a layout: </span>
+        <input
+          type="radio"
+          name="view"
+          id="teacher-view"
+          value="teacher"
+          v-model="selectedView"
+          class="tab"
+        />
+        <label for="teacher-view" class="tab-text">Teacher</label>
+        <input
+          type="radio"
+          name="view"
+          id="student-view"
+          class="tab"
+          value="student"
+        />
+        <label for="student-view" class="tab-text">Student</label>
+      </form>
+      <AdminItems v-if="selectedView === 'teacher'" />
+      <StudentItems v-if="selectedView === 'student'" />
     </div>
   </section>
 </template>
 
 <script>
+import StudentItems from "@/components/Lobby/StudentItems.vue";
+import AdminItems from "@/components/Lobby/AdminItems.vue";
 export default {
   name: "Home",
+  data() {
+    return {
+      selectedView: "teacher",
+    };
+  },
+  components: {
+    StudentItems,
+    AdminItems,
+  },
+  computed: {
+    isStudent() {
+      return this.$store.getters.isStudent || this.$store.getters.isAlumni;
+    },
+    isTeacher() {
+      return this.$store.getters.isTeacher;
+    },
+  },
 };
 </script>
 
-<style lang="css">
-.home {
-  margin-top: 4rem;
+<style>
+.view-wrapper {
+  margin-block: 1rem;
+
+  display: flex;
+  gap: 1rem;
 }
 
 .home__nav {
@@ -161,7 +86,6 @@ export default {
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  transition: background-color 0.25s ease-in-out, color 0.25s ease-in-out;
 }
 
 .home__link:hover {
