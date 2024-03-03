a68e7ab717e8c213092d80c1604d7bc653e60336
Ferdinand Niemann <hello@ferdi.digital>
2024-03-03 21:12:00 +0100

    style: update lobby styling and layout

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -5,39 +5,37 @@
         Hello, {{ this.$store.getters.userName }} - Let's go!
       </p>
     </header>
-    <div class="student-wrapper" v-if="isStudent">
-      <StudentItems />
-    </div>
-    <div class="teacher-wrapper" v-if="isTeacher">
-      <form class="view-wrapper" @change="selectedView = $event.target.value">
-        <span>Choose a layout: </span>
-        <input
-          type="radio"
-          name="view"
-          id="teacher-view"
-          value="teacher"
-          v-model="selectedView"
-          class="tab"
-        />
-        <label for="teacher-view" class="tab-text">Teacher</label>
-        <input
-          type="radio"
-          name="view"
-          id="student-view"
-          class="tab"
-          value="student"
-        />
-        <label for="student-view" class="tab-text">Student</label>
-      </form>
-      <AdminItems v-if="selectedView === 'teacher'" />
-      <StudentItems v-if="selectedView === 'student'" />
-    </div>
+    <form
+      class="view-wrapper"
+      @change="selectedView = $event.target.value"
+      v-if="isTeacher"
+    >
+      <span>Choose a layout: </span>
+      <input
+        type="radio"
+        name="view"
+        id="teacher-view"
+        value="teacher"
+        v-model="selectedView"
+        class="tab"
+      />
+      <label for="teacher-view" class="tab-text">Teacher</label>
+      <input
+        type="radio"
+        name="view"
+        id="student-view"
+        class="tab"
+        value="student"
+        v-model="selectedView"
+      />
+      <label for="student-view" class="tab-text">Student</label>
+    </form>
+    <ItemsGrid :view="selectedView" />
   </section>
 </template>
 
 <script>
-import StudentItems from "@/components/Lobby/StudentItems.vue";
-import AdminItems from "@/components/Lobby/AdminItems.vue";
+import ItemsGrid from "@/components/Lobby/ItemsGrid.vue";
 export default {
   name: "Home",
   data() {
@@ -46,13 +44,9 @@ export default {
     };
   },
   components: {
-    StudentItems,
-    AdminItems,
+    ItemsGrid,
   },
   computed: {
-    isStudent() {
-      return this.$store.getters.isStudent || this.$store.getters.isAlumni;
-    },
     isTeacher() {
       return this.$store.getters.isTeacher;
     },
@@ -60,46 +54,11 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .view-wrapper {
   margin-block: 1rem;
 
   display: flex;
   gap: 1rem;
-}
-
-.home__nav {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 1rem;
-  justify-content: center;
-}
-
-.home__link {
-  background: #e5e5e5;
-  box-shadow: 1px 1px 9px 1px #c4c4c4;
-  border-radius: 5px;
-  padding: 1rem 3rem;
-  color: #292929;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.home__link:hover {
-  background-color: #595959;
-  color: #ffffff;
-}
-
-.home__link > svg {
-  color: rgba(38, 38, 38, 0.4);
-  width: 2rem;
-  margin-bottom: 1rem;
-}
-
-.home__link:hover > svg {
-  color: rgba(242, 242, 242, 0.57);
 }
 </style>
