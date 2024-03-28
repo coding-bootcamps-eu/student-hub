916233b3cfdf19291c90cb988b8cbd9560972acc
Ferdinand Niemann <hello@ferdi.digital>
2024-03-28 10:04:59 +0100

    add logo to main header

--- src/components/Header/MainHeader.vue
+++ src/components/Header/MainHeader.vue
@@ -1,8 +1,14 @@
 <template>
   <header class="main-header">
-    <div class="header__content">
-      <h1 class="header__title">Student Hub</h1>
-      <p class="header__subtitle">Coding Bootcamps Europe</p>
+    <div class="header-content">
+      <img
+        src="/img/Logo_V2_rund.png"
+        alt="Logo Coding Bootcamps Europe GmbH"
+        class="header-content__logo" />
+      <div class="header-content__text">
+        <h1 class="header-content__title">Student Hub</h1>
+        <p class="header-content__subtitle">Coding Bootcamps Europe</p>
+      </div>
     </div>
     <MainNavigation />
   </header>
