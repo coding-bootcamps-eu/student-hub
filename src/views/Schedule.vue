65fb4aed0942ba350dfc6f485880a6b96da1aeb4
Ferdinand Niemann <hello@ferdi.digital>
2024-06-18 08:41:15 +0200

    remove event listener for screen resizing on schedule page

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -45,76 +45,6 @@
         </tbody>
       </table>
     </div>
-    <ul class="schedule-list" v-if="isMobile">
-      <li
-        class="module-card card card-accent"
-        v-for="module in selectedSchedule"
-        :key="module.title"
-      >
-        <header
-          class="module__header"
-          @click="module.isShown = !module.isShown"
-        >
-          <div class="module__header-content">
-            <h3 class="module-title">{{ module.title }}</h3>
-            <p class="module-length">Length: {{ module.length }}</p>
-          </div>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            class="toggle-accordeon"
-            viewBox="0 0 16 16"
-            v-if="!module.isShown"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
-            />
-          </svg>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="currentColor"
-            class="toggle-accordeon"
-            viewBox="0 0 16 16"
-            v-if="module.isShown"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"
-            />
-          </svg>
-        </header>
-        <ul class="categories" v-show="module.isShown">
-          <li
-            class="category"
-            v-for="category of module.categories"
-            :key="category"
-          >
-            <p class="category-title">{{ category.title }}</p>
-            <div class="category-links">
-              <a
-                :href="category.videos"
-                v-if="category.videos"
-                class="category-link"
-                >Videos</a
-              >
-              <a
-                :href="category.tasks"
-                v-if="category.tasks"
-                class="category-link"
-                >Tasks</a
-              >
-              <a
-                :href="category.slides"
-                v-if="category.slides"
-                class="category-link"
-                >Slides</a
-              >
-            </div>
-          </li>
-        </ul>
-      </li>
-    </ul>
   </div>
 </template>
 <script>
@@ -141,10 +71,6 @@ export default {
     } else {
       this.selectedClass = "Teilzeit";
     }
-
-    this.screenSize = window.innerWidth;
-
-    window.addEventListener("resize", this.changeScreenSize);
   },
   computed: {
     selectedSchedule() {
@@ -153,14 +79,6 @@ export default {
       } else {
         return this.fullTimeSchedule;
       }
-    },
-    isMobile() {
-      return this.screenSize < 768;
-    },
-  },
-  methods: {
-    changeScreenSize() {
-      this.screenSize = window.innerWidth;
     },
   },
 };
--- src/views/Students.vue
+++ src/views/Students.vue
@@ -181,32 +181,15 @@
                 rel="noopener noreferrer"
                 class="student__github-link"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="currentColor"
-                  class="icon icon-github"
-                  viewBox="0 0 16 16"
-                >
-                  <path
-                    d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"
-                  />
-                </svg>
-                {{ getShortLink(student.data.githubProfileUrl) }}</a
-              >
+                <GitHubIcon />
+                {{ getShortLink(student.data.githubProfileUrl) }}
+              </a>
             </td>
             <td class="col-edit">
-              <svg
+              <PenIcon
                 @click="student.editMode = !student.editMode"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                class="icon icon-edit"
                 v-if="!student.editMode"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"
-                />
-              </svg>
+              />
             </td>
           </tr>
         </tbody>
@@ -217,6 +200,8 @@
 
 <script>
 import PageHeader from "@/components/PageHeader.vue";
+import GitHubIcon from "@/components/icons/GitHubIcon.vue";
+import PenIcon from "@/components/icons/PenIcon.vue";
 import {
   query,
   where,
@@ -238,6 +223,8 @@ export default {
 
   components: {
     PageHeader,
+    GitHubIcon,
+    PenIcon,
   },
 
   data() {
